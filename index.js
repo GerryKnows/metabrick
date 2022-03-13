0bd94df84e0110ef1ed1df418478af8ccd1191da
gerrythezombie <ldggerardorm@gmail.com>
2022-03-12 17:43:42 -0700

    animación gráfica

--- index.js
+++ index.js
@@ -1,29 +1,80 @@
 
 var ctx = document.getElementById("myChart").getContext("2d");
-var myChart = new Chart(ctx,{
+let delayed;
+
+//Gradient Fill
+let gradient = ctx.createLinearGradient(0,0,0,400)
+gradient.addColorStop(0,"rgb(158,118,46)");
+gradient.addColorStop(1,"rgb(233,202,135)");
+
+
+
+
+const labels = [
+    '23-feb ',
+    '24-feb',
+    '25-feb',
+    '26-feb', 
+    '27-feb', 
+    '28-feb',
+    '1-mar',
+    '2-mar'
+]
+
+const data = {
+    labels,
+    datasets: [{
+        label:'Valor de la accion de metabrick', 
+        data:[100.00,116.00,113.00,114.00,114.50,106.00,105.00,105.00, ],
+        fill:true,
+        backgroundColor:gradient,
+        borderColor:'rgb(233,202,135)',
+        color:'rgb (255,255,255)',
+        pointBackgroundColor:"#000",
+        tension:.3,
+
+    }]
+}
+
+const config = {
     type:"line",
-    data:{
-        labels: ['23-feb ','24-feb','25-feb','26-feb', '27-feb', '28-feb'],
-        datasets: [{
-            label:'num datos', 
-            data:[100.00,116.00,113.00,114.00,114.50,106.00 ],
-            backgroundColor:[
-                'rgb(66,134,244)',
-                'rgb(74,135,72)',
-                'rgb(229,89,50)'
-            ],
-
-            borderColor:[
-                'rgb(0,0,0)'
-            ]
-
-        }]
-    },
+    data: data,
     options:{ 
+        radius:5,
+        hitRadius:30,
+        responsive:true,
+        animation: {
+            onComplete: () => {
+              delayed = true;
+            },
+            delay: (context) => {
+              let delay = 0;
+              if (context.type === 'data' && context.mode === 'default' && !delayed) {
+                delay = context.dataIndex * 300 + context.datasetIndex * 100;
+              }
+              return delay;
+            },
+          },
         scales:{
-            y:{     
-                beginAtZero: true                
-            }
-        }
+            y:{    
+                              
+                suggestedMin: 90,
+                suggestedMax: 130,
+                ticks: {
+                    callback: function (value,){
+                        return "$" + value 
+                    }
+                    
+                }   
+            }          
+        },
     }
-});
+}
+    
+
+
+const myChart = new Chart(ctx, config);
+   
+   
+
+

7502d2eecd8e6b2c34a6330adeb7f5426f8775dd
gerrythezombie <ldggerardorm@gmail.com>
2022-03-21 18:12:56 -0700

    tabla

--- criptoanalisis_api.js
+++ criptoanalisis_api.js
@@ -1,66 +1,51 @@
 
-const API_URL = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD,EUR';
 
-const HTMLResponse = document.querySelector("#app");
-const ul = document.createElement('ul');
-
-
-fetch (`${API_URL}`)
-.then((response) => response.json())
-.then((users) => {
-  Object.keys(users).forEach(BTC => {
-        let elem = document.createElement('li');
-        elem.appendChild(document.createTextNode(`Bitcoin a Dollar: ${users[BTC].USD} Bitcoin a Euro: ${users[BTC].EUR}`)
-        );
-        ul.appendChild(elem);
-    });
-    HTMLResponse.appendChild(ul);
-});
 
 // 👇️ with Objects
-const obj = {name: 'Tom', country: 'Chile'};
+// const obj = {name: 'Tom', country: 'Chile'};
 
-Object.keys(obj).forEach(key => {
-  console.log(key); // 👉️ "name", "country"
-  console.log(obj[key]); // 👉️ "Tom", "Chile"
-});
+// Object.keys(obj).forEach(key => {
+//   console.log(key); // 👉️ "name", "country"
+//   console.log(obj[key]); // 👉️ "Tom", "Chile"
+// });
 
 
   // console.log( `Ethereum: ${json.ETH.USD} Bitcoin: ${json.BTC.USD } Solana: ${json.SOL.USD }`))
 
-  fetch('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,SOL&tsyms=USD,EUR')
+  
+
+  fetch('https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD')
   .then(response => response.json())
   .then(cosa => console.log(cosa))
 
-  fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(cosa => console.log(cosa))
+
+  // fetch('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,SOL&tsyms=USD,EUR')
+  // .then(response => response.json())
+  // .then(cosa => console.log(cosa))
+
+  // fetch('https://jsonplaceholder.typicode.com/users')
+  // .then(response => response.json())
+  // .then(cosa => console.log(cosa))
 
 
-const API_URL_2 = 'https://jsonplaceholder.typicode.com';
+// const API_URL_2 = 'https://jsonplaceholder.typicode.com';
 
 
-const HTMLResponse_2 = document.querySelector("#app");
-const ull = document.createElement('ul');
+// const HTMLResponse_2 = document.querySelector("#app");
+// const ull = document.createElement('ul');
 
 
-fetch (`${API_URL_2}/users`)
-.then((response) => response.json())
-.then((users) => {
-    users.forEach(user => {
-        let elem = document.createElement('li');
-        elem.appendChild(document.createTextNode(`Nombre: ${user.name} Correo: ${user.email} aidi: ${user.id}`)
-        );
-        ull.appendChild(elem);
-    });
-    HTMLResponse_2.appendChild(ull);
-});
-
-
-
-
-
-
+// fetch (`${API_URL_2}/users`)
+// .then((response) => response.json())
+// .then((users) => {
+//     users.forEach(user => {
+//         let elem = document.createElement('li');
+//         elem.appendChild(document.createTextNode(`Nombre: ${user.name} Correo: ${user.email} aidi: ${user.id}`)
+//         );
+//         ull.appendChild(elem);
+//     });
+//     HTMLResponse_2.appendChild(ull);
+// });
 
 
 // STRINGS PARA INTRODUCIR HTML
@@ -71,9 +56,6 @@ fetch (`${API_URL_2}/users`)
 //     const tpl = users.map((user) => `<li> ${user.name} <3 ${user.email } </li>`);
 //     HTMLResponse.innerHTML = `<ul>${tpl}</ul>`;
 // });
-
-
-
 
 
 // Consumir API antes de ECMASCRIPT 6
@@ -98,3 +80,76 @@ fetch (`${API_URL_2}/users`)
 // xhr.addEventListener('load', onRequestHandler)
 // xhr.open('GET', `${API_URL}/users`);
 // xhr.send();
+
+// var contenido = document.querySelector('#contenido')
+
+// function traer(){
+//   const API_URL = 'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD';
+
+// const HTMLResponse = document.querySelector("#app");
+// const td = document.createElement('td');
+// const td_2 = document.createElement('td');
+
+
+// fetch (`${API_URL}`)
+// .then((response) => response.json())
+// .then((users) => {
+//   Array.from(users.Data).forEach(BTC => {
+//         console.log(BTC);
+//         let elem = document.createElement('tr');
+//         elem.appendChild(document.createTextNode(`${BTC.CoinInfo.Name}`) ,
+//         // elem.appendChild(document.createTextNode(` Precio Dll: $${BTC.RAW.USD.PRICE}`) ,
+//         // elem.appendChild(document.createTextNode(` Market Cap:T $${BC.RAW.USD.MKTCAP}`)         
+//         // ))
+//         );
+//         td.appendChild(elem);
+//         console.log(elem);
+//   });
+
+//   Array.from(users.Data).forEach(BTC => {
+//       console.log(BTC);
+//       let price = document.createElement('tr');
+//       price.appendChild(document.createTextNode(`$${BTC.RAW.USD.PRICE}`) ,
+//       );
+//       td_2.appendChild(price);
+//       console.log(price);
+//   });
+//     HTMLResponse.appendChild(td);
+//     HTMLResponse.appendChild(td_2);
+   
+// });
+
+// }
+
+var contenido = document.querySelector('#contenido')
+
+function traer(){
+  const API_URL = 'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD';
+
+  fetch (`${API_URL}`)
+      .then((response) => response.json())
+      .then((users) => {
+          // console.log(datos.Data)
+        tabla(users)     
+    });
+}
+
+function tabla(users) {
+  // console.log(users.Data)
+  contenido.innerHTML = ''
+  for(let valor of users.Data) {
+    // console.log(valor.CoinInfo.Name)
+    contenido.innerHTML += `
+    <tr>
+         <th scope="row">
+         <img src="${valor.CoinInfo.ImageUrl}" class="img-thumbnail" alt="...">
+         </th>
+         <td>${valor.CoinInfo.Name}</td>
+         <td> $${valor.RAW.USD.PRICE}</td>
+         <td>$${valor.RAW.USD.MKTCAP}</td>
+    </tr>
+
+    `
+  }
+
+}
